e4c455b22c015b58f4c52e7d482b76bba8fe73f6
Crowdbotics <team@crowdbotics.com>
2021-10-25 23:26:10 +0000

    Initial commit.

---
+++ backend/branded_era_musiq_r_31699/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for branded_era_musiq_r_31699 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'branded_era_musiq_r_31699.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "branded_era_musiq_r_31699",
+  url: "https://branded_era_musiq_r_31699.botics.co",
+  api: "https://branded_era_musiq_r_31699.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
